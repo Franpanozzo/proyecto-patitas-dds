52108604d561e683447462af7bb1691fc55bfa55
Franpanozzo <francisco.panozzosf@gmail.com>
2021-05-31 18:46:50 -0300

    Falta requerimiento 5, tests y diagrama

---
+++ src/main/java/Servicios/Hogares/Admision.java
@@ -0,0 +1,14 @@
+package Servicios.Hogares;
+
+public class Admision {
+  boolean perros;
+  boolean gatos;
+
+  public boolean getPerros(){
+    return perros;
+  }
+
+  public boolean getGatos(){
+    return gatos;
+  }
+}
