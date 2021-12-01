f82f6a22c2d0e456a9d8c19b48f5e658d40df17b
Franpanozzo <francisco.panozzosf@gmail.com>
2021-12-01 16:08:57 -0300

    Cambios para heroku -8

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,8 +6,20 @@ import spark.debug.DebugScreen;
 public class Server {
   public static void main(String[] args) {
     //Bootstrap.getInstance().init();
-    Spark.port(Integer.parseInt(System.getenv("PORT")));
+    //Spark.port(Integer.parseInt(System.getenv("PORT")));
+    Spark.port(getHerokuAssignedPort());
     Router.init();
     DebugScreen.enableDebugScreen(); //TODO: No hay que tener esta linea cuando entreguemos
+    Spark.staticFileLocation("/src/resources");
   }
+
+  static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+  }
+
 }
